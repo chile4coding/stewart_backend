641fb5a17f1b30163a7a09c6635998353a989e94
chile4coding <chileomereji@gmail.com>
2023-11-18 08:45:26 +0100

    fix now

--- build/middleware/requestHeader.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const requestHeaders = (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-};
-exports.default = requestHeaders;
